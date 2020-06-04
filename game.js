26e6f69abecccb839068b65b2a0f78fd1c22221c
Grasshopper <mikaela301@gmail.com>
2020-06-03 20:30:39 -0700

    Added button animation to user clicks

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
+//on click function that stores user clicked button pattern in userClickedPattern array
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColor);
+
+    animateButton(userChosenColor);
+
+    playSound(userChosenColor);
+})
+
+//generates random button color pattern. FLash animation.
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 
@@ -8,9 +21,25 @@ function nextSequence() {
 
     gamePattern.push(randomChosenColor);
 
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + currentColor).fadeIn(100).fadeOut(100).fadeIn(100); 
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
+    playSound(randomChosenColor);
 }
 
+// plays button sounds
+function playSound(name) {
+
+     var audio = new Audio("sounds/" + name + ".mp3");
+     audio.play();
+}
+
+//pressed animation for user clicked buttons
+function animateButton(currentColor) {
+
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+
+}
