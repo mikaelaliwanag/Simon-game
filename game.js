48ba61d5b8e6d608a9ce9d26e5ace829ad370fe9
Grasshopper <mikaela301@gmail.com>
2020-06-05 18:00:08 -0700

    Added function to check user answer against game pattern

--- game.js
+++ game.js
@@ -1,38 +1,53 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-var level = 1;
+var level = 0;
 
 //keypress event that starts game
 $(document).one("keydown", function() {
 
     $("#level-title").text("Level " + level);
     nextSequence();
-
 });
 
 //on click function that stores user clicked button pattern
 $(".btn").click(function() {
-    var userChosenColor = $(this).attr("id");
 
+    var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
 
     animateButton(userChosenColor);
-
     playSound(userChosenColor);
 
+    checkAnswer(userClickedPattern.length - 1);
 });
+
+
+function checkAnswer(currentLevel) {
+
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+               nextSequence();
+            }, 1000);
+      }
+
+    } else {
+        
+    }
+}
 
 //generates random button color pattern
 function nextSequence() {
-    level++;
 
-    $("level-title").text("Level " + level);
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
 
     var randomNumber = Math.floor(Math.random() * 4);
-
     var randomChosenColor = buttonColors[randomNumber];
-
     gamePattern.push(randomChosenColor);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100); 
@@ -58,7 +73,6 @@ function animateButton(currentColor) {
     }, 100);
 
 }
-
 
 
 
